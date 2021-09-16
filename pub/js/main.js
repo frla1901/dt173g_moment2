0c8d16781481789c75bb34716351b297fcc1e9d4
FriLazz <frida.lazzari@gmail.com>
2021-09-16 13:14:54 +0200

    /pub to gitHub

---
+++ pub/js/main.js
@@ -0,0 +1,2 @@
+function buttonFunction(){location.replace("https://github.com/frla1901/dt173g_moment2.git")}var date=new Date,options={weekday:"long",month:"long",day:"numeric",year:"numeric"};date=date.toLocaleString("sv-SE",options),document.getElementById("date").innerHTML=date;
+//# sourceMappingURL=../maps/main.js.map
